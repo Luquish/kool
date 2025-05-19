08b15159e83ccd2407bf2f597d3a5ccce96d3bc1
Luquish <84946392+Luquish@users.noreply.github.com>
2025-05-19 16:55:56 -0300

    fix errors

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from "react"
 import Link from "next/link"
-import { Info } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
